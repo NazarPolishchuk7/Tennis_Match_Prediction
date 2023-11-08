c8207130e9bdf9f886e7ef8531d7bac30ac04c36
NazarPolishcuk7 <nazar.polishchuk07@gmail.com>
2023-11-08 19:05:36 +0200

    simple model update

--- utils/BetAnalyzer.py
+++ utils/BetAnalyzer.py
@@ -1,7 +1,26 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Oct 20 18:02:36 2023
+def code_morze(value):
+    '''
+    please add your solution here or call your solution implemented in different function from here
+    then change return value from 'False' to value that will be returned by your solution
+    '''
+    morze_code_dict = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.',
+     'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.',
+      'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--',
+      'Z': '--..', '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....', '6': '-....', '7': '--...',
+       '8': '---..', '9': '----.', '0': '-----', ', ': '--..--', '.': '.-.-.-', '?': '..--..', '/': '-..-.', '-': '-....-',
+        '(': '-.--.', ')': '-.--.-'}
+    value = value.upper()
 
-@author: nazar
-"""
-print("Hello world")
+    result = []
+
+    for char in value:
+        if char == '':
+            result.append('')
+        elif char in morze_code_dict:
+            result.append(morze_code_dict[char])
+
+    return ' '.join(result)
+
+value = 'Data Science-2022'
+
+print(code_morze(value))
